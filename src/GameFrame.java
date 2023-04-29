29eedd27296c917de10f67f13090eb8b143eba1c
hungvu19061991@gmail.com <Comebacktome91>
2023-04-29 23:26:51 +0700

    âsasas

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-    GameFrame(){
-
+    GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
